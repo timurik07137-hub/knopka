3a524f43788f8d674e3f6729ad67f5acfcb87545
timurik07137-hub <timurik07137@gmail.com>
2026-06-29 23:51:58 +0300

    Initial commit - Задание 7

---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace knopka
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
